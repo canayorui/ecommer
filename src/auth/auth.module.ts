c9c8209819316879dc1249add1d36bb6a32ecf20
canayorui <canayoruiz28@gmail.com>
2025-10-31 09:24:29 -0300

    agregar soporte para TypeORM en AuthModule y hacer que el método signIn sea asíncrono en AuthService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersRepository } from 'src/users/users.repository';
+import { Users } from 'src/entities/users.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Users])],
   controllers: [AuthController],
   providers: [AuthService, UsersRepository],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,11 +8,11 @@ export class AuthService {
     return 'auth funciona';
   }
 
-  signIn(email: string, password: string) {
+  async signIn(email: string, password: string) {
     if (!email || !password) {
       return 'Email y password son obligatorios';
     }
-    const user = this.usersRepository.getUserByEmail(email);
+    const user = await this.usersRepository.getUserByEmail(email);
 
     if (!user || user.password !== password) {
       return 'Credenciales invalidas';
